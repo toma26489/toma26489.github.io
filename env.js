47c08cde3ccd70910dc7ac71392fe956f45f0a11
toma26489 <tomaledent@gmail.com>
2026-02-04 15:47:11 +0100

    Mon premier push vers GitHub Pages

---
+++ env.js
@@ -0,0 +1,7 @@
+export const env = {
+    API_KEY: 'e29a2cee4208640874c4b0e82ec71877',
+    GEO_API_URL: 'https://api.openweathermap.org/geo/1.0/direct',
+    WEATHER_API_URL: 'https://api.openweathermap.org/data/2.5/forecast',
+    RECENT_CITIES_KEY: 'weatherAppRecentCities',
+    MAX_RECENT_CITIES: 5
+};
